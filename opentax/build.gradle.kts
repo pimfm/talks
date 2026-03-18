f73a7b76537d2c25babbffcb5ccfc0e905b9dc7e
Pim van Gurp <gurp@duck.com>
2026-03-18 23:36:09 +0100

    Initial commit

--- opentax/build.gradle.kts
+++ opentax/build.gradle.kts
@@ -7,7 +7,7 @@ group = "fm.pim"
 version = "0.0.1"
 
 application {
-    mainClass = "io.ktor.server.netty.EngineMain"
+    mainClass = "fm.pim.ApplicationKt"
 }
 
 kotlin {
@@ -19,7 +19,6 @@ dependencies {
     implementation(libs.ktor.server.netty)
     implementation(libs.logback.classic)
     implementation(libs.ktor.server.core)
-    implementation(libs.ktor.server.config.yaml)
     testImplementation(libs.ktor.server.test.host)
     testImplementation(libs.kotlin.test.junit)
 }
--- opentax/src/main/kotlin/Application.kt
+++ opentax/src/main/kotlin/Application.kt
@@ -1,9 +1,17 @@
 package fm.pim
 
 import io.ktor.server.application.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
 
-fun main(args: Array<String>) {
-    io.ktor.server.netty.EngineMain.main(args)
+fun main() {
+    embeddedServer(
+        Netty,
+        port = 8080,
+        host = "0.0.0.0",
+        module = Application::module
+    )
+        .start(wait = true)
 }
 
 fun Application.module() {
